4093ace4d6b08fb54531ac5f52442e7e48b3197a
Your Name <you@example.com>
2022-11-08 17:45:28 -0800

    [Add] clean up code and header file

--- shell.c
+++ shell.c
@@ -1,34 +1,89 @@
 #include "shell.h"
+
 /**
- * _isatty - verif terminal is in interactive mode
+ * sig_handler - checks if Ctrl C is pressed
+ * @sig_num: int
  */
-void _isatty(void)
+void sig_handler(int sig_num)
 {
-  if (isatty(STDIN_FILENO)) /*return 1 if the file in the argument refers to a terminal*/
+  if (sig_num == SIGINT)
     {
-    _puts("#cisfun$ ");
+      _puts("\n#cisfun$ ");
     }
 }
 
-
-
 /**
- * main - Shell entry point
- * Return: 0 on success
- * @ac: number of arguments passed
- * @arv: Argument values 
-*/
-
-int main(int ac __attribute__((unused)), char **arv __attribute__((unused)))
+ * _EOF - handles the End of File
+ * @len: return value of getline function
+ * @buff: buffer
+ */
+void _EOF(int len, char *buff)
 {
-  char *buffer = NULL; /*this will be used to get the first character in the user entered string and stores the total string entered*/
-  size_t bufsize = 0; /*holds the size of the input buffer*/
-  ssize_t characters; /*returns the number of characters in the user entered string*/
-  while (characters != EOF)
-  {
-    _isatty(); /*enter interactive terminal mode*/
-    characters = getline(&buffer, &bufsize, stdin); /*store the user input, stdin reads the standard input*/
-  }
-_puts(buffer);
-return (0);
+  (void)buff;
+  if (len == -1)
+    {
+      if (isatty(STDIN_FILENO))
+	{
+	  _puts("\n");
+	  free(buff);
+	}
+      exit(0);
+    }
+}
+/**
+ * _isatty - verif if terminal
+ */
+
+void _isatty(void)
+{
+  if (isatty(STDIN_FILENO))
+    _puts("#cisfun$ ");
+}
+/**
+ * main - Shell
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+  ssize_t len = 0;
+  char *buff = NULL, *value, *pathname, **arv;
+  size_t size = 0;
+  list_path *head = '\0';
+  void (*f)(char **);
+
+  signal(SIGINT, sig_handler);
+  while (len != EOF)
+    {
+      _isatty();
+      len = getline(&buff, &size, stdin);
+      _EOF(len, buff);
+      arv = stringparser(buff, " \n");
+      if (!arv || !arv[0])
+	execute(arv);
+      else
+	{
+	  value = _getenv("PATH");
+	  head = linkpath(value);
+	  pathname = _which(arv[0], head);
+	  f = checkbuild(arv);
+	  if (f)
+	    {
+	      free(buff);
+	      f(arv);
+	    }
+	  else if (!pathname)
+	    execute(arv);
+	  else if (pathname)
+	    {
+	      free(arv[0]);
+	      arv[0] = pathname;
+	      execute(arv);
+	    }
+	}
+    }
+  free_list(head);
+  freearv(arv);
+  free(buff);
+  return (0);
 }
